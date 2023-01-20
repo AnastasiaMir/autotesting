9af197d797f82ff59a434d5ba440e97076bd6930
Anastasia Mironova <anastasia.mironova7@gmail.com>
2023-01-20 20:52:44 +0300

    tests with strictasscert

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,8 +1,8 @@
+import { strict as assert } from 'node:assert';
 import { capitalize } from '../src/capitalize.js'
-if (capitalize('word') !== 'Word') { 
-    throw new Error('Функция работает неверно!');
-  }
-if (capitalize('') !== '') {
-    throw new Error('Функция работает неверно!');
-}
+
+assert.equal(capitalize(''), '');
+
+
+assert.equal(capitalize('hello'), 'Hello');
 console.log('Все тесты пройдены!');
